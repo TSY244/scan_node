1417bca078b73c69bca354f00610060209430e02
TSY244 <1241407552@qq.com>
2024-03-07 20:37:36 +0800

    🦄 refactor: 修改了部分函数的传参方式

--- client.py
+++ client.py
@@ -22,12 +22,12 @@ loguru.logger.add("/log/error.log", rotation="500 MB", retention="10 days", leve
 
 
 
-g_debug=0
+g_debug=1
 g_use_ips=[]
 g_time=0
 
 def signal_handler(signal, frame):
-    loguru.logger.error("exit")
+    loguru.logger.error("exit,beacuse of {} signal".format(signal))
     pid=os.getpid()
     os.kill(pid,signal)
 
@@ -40,7 +40,6 @@ def get_config():
     redis_port = config['REDIS']['redis_port']
     redis_password = config['REDIS']['redis_password']
     message_name = config['REDIS']['message_queue']
-
     redis={
         "redis_host":redis_host,
         "redis_port":redis_port,
@@ -63,22 +62,43 @@ def get_config():
     # web_path_scan
     wpc_mode=config['web_path_scanner']['mode']
     wpc_thread=config['web_path_scanner']['thread']
-    web_path_scan={
-        "mode":wpc_mode,
-        "thread":int(wpc_thread)
-    }
+    if wpc_mode=="file":
+        wpc_file_name=config['web_path_scanner']['file_name']
+        web_path_scan={
+            "mode":wpc_mode,
+            "file_name":wpc_file_name,
+            "thread":int(wpc_thread)
+        }
+    elif wpc_mode=="dir":
+        wpc_file_path=config['web_path_scanner']['file_path']
+        web_path_scan={
+            "mode":wpc_mode,
+            "file_path":wpc_file_path,
+            "thread":int(wpc_thread)
+        }
+    else:
+        raise Exception("web_path_scanner mode is error")
 
     # subdomain_scanner
-    subdomain_file_path=config['subdomain_scanner']['file_path']
-    subdomain_file_name=config['subdomain_scanner']['file_name']
     subdomain_thread=config['subdomain_scanner']['thread']
-    subdomain_scan={
-        "file_path":subdomain_file_path,
-        "file_name":subdomain_file_name,
-        "thread":int(subdomain_thread)
-    }
-        
-
+    subdomain_mode=config['subdomain_scanner']['mode']
+    if subdomain_mode=="file":
+        subdomain_file_name=config['subdomain_scanner']['file_name']
+        subdomain_scan={
+            "mode":subdomain_mode,
+            "file_name":subdomain_file_name,
+            "thread":int(subdomain_thread)
+        }
+    elif subdomain_mode=="dir":
+        subdomain_file_path=config['subdomain_scanner']['file_path']
+        subdomain_scan={
+            "mode":subdomain_mode,
+            "file_path":subdomain_file_path,
+            "thread":int(subdomain_thread)
+        }
+    else:
+        raise Exception("subdomain_scanner mode is error")
+    
     return redis,es,web_path_scan,subdomain_scan
 
 def get_ip(redis: my_redis.Redis):
@@ -124,20 +144,69 @@ def check_es(es:dict):
         raise Exception("es_vuls_index is invalid")
     return True
 
-def check_web_path_scan(web_path_scan:dir):
-    if web_path_scan==None:
-        loguru.logger.error("web_path_scan is None")
-        raise Exception("web_path_scan is None")
-    if "mode" not in web_path_scan or "thread" not in web_path_scan:
-        loguru.logger.error("web_path_scan is error")
-        raise Exception("web_path_scan is error")
-    if web_path_scan["mode"]!="file" and web_path_scan["mode"]!="dir":
-        loguru.logger.error("web_path_scan mode is error")
-        raise Exception("web_path_scan mode is error")
+
+def check_web_path_scan(web_path_scan:dir): # delete you can use check_scanner
+    if len(web_path_scan)!=3:
+        raise Exception("web_path_scan config error")
+    if web_path_scan["mode"] not in ["file","dir"]:
+        raise Exception("web_path_scan mode is invalid")
     if not isinstance(web_path_scan["thread"],int):
-        loguru.logger.error("web_path_scan thread is error")
-        raise Exception("web_path_scan thread is error")
+        raise Exception("web_path_scan thread is invalid")
+    if web_path_scan["mode"]=="file" and web_path_scan["file_name"]==None:
+        raise Exception("web_path_scan file_name is None")
+    if web_path_scan["mode"]=="dir" and web_path_scan["file_path"]==None:
+        raise Exception("web_path_scan file_path is None")
     return True
+
+def check_subdomain_scan(subdomain_scan:dir): # delete you can use check_scanner
+    if len(subdomain_scan)!=3:
+        raise Exception("subdomain_scan config error")
+    if subdomain_scan["mode"] not in ["file","dir"]:
+        raise Exception("subdomain_scan mode is invalid")
+    if not isinstance(subdomain_scan["thread"],int):
+        raise Exception("subdomain_scan thread is invalid")
+    if subdomain_scan["mode"]=="file" and subdomain_scan["file_name"]==None:
+        raise Exception("subdomain_scan file_name is None")
+    if subdomain_scan["mode"]=="dir" and subdomain_scan["file_path"]==None:
+        raise Exception("subdomain_scan file_path is None")
+    return True
+
+# todo check if exist path or file 
+def check_scanner(scanner:dir):
+    '''
+    check scanner config, you can use web_path_scan ,subdomain_scan
+    '''
+    if len(scanner)!=3:
+        raise Exception(f"scanner {scanner} config error")
+    if scanner["mode"] not in ["file","dir"]:
+        raise Exception(f"scanner {scanner} mode is invalid")
+    if not isinstance(scanner["thread"],int):
+        raise Exception(f"scanner {scanner} thread is invalid")
+    if scanner["mode"]=="file" and scanner["file_name"]==None:
+        raise Exception(f"scanner {scanner} file_name is None")
+    if scanner["mode"]=="dir" and scanner["file_path"]==None:
+        raise Exception(f"scanner {scanner} file_path is None")
+    return True
+
+def check_redis(redis_host, redis_port,redis_password):
+    # each 5 seconds, try to connect redis, and connect 100 times 
+    now_times=0
+    max_times=100
+
+    ip_tools.check_ip_if_valid(redis_host)
+
+    try:
+        while now_times<max_times:
+            if my_redis.Redis.check_if_connectable(redis_host, redis_port, redis_password):
+                return
+            loguru.logger.info("can't connect redis")
+            now_times+=1
+            time.sleep(5)
+        raise Exception
+    except:
+        loguru.logger.error("redis connect error")
+        sys.exit(1)
+
 
 def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:dir=None):
     '''
@@ -152,8 +221,11 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
         key                   vlues
         mode:str              str     #file or dir
         thread:int            int     #thread number
+        file_path:str         str     #file path
+        file_name:str         str     #file name
     subdomain_scan is a dir
         key                   vlues
+        mode:str              str     #file or dir
         file_path:str         str     #file path
         file_name:str         str     #file name
         thread:int            int     #thread number
@@ -166,15 +238,28 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
     except Exception as e:
         loguru.logger.error(e)
         sys.exit(1)
+    es["es_port"]=int(es["es_port"]) if type(es["es_port"])==str else es["es_port"]
 
-    # check web_path_scan
+    # # check web_path_scan
+    # try:
+    #     check_web_path_scan(web_path_scan)
+    # except Exception as e:
+    #     loguru.logger.error(e)
+    #     sys.exit(1)
+
+    # check web path scan
     try:
-        check_web_path_scan(web_path_scan)
+        check_scanner(web_path_scan)
     except Exception as e:
         loguru.logger.error(e)
         sys.exit(1)
-
-    es["es_port"]=int(es["es_port"]) if type(es["es_port"])==str else es["es_port"]
+    
+    # check subdomain_scan
+    try:
+        check_scanner(subdomain_scan)
+    except Exception as e:
+        loguru.logger.error(e)
+        sys.exit(1)
 
     # ip size is 10
     while True:
@@ -204,44 +289,59 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
 
         # domain 
         value2domian=domain.run(value)
-
+        if g_debug==1:
+            loguru.logger.info(f"get domain ==> domain is {value2domian}")
 
         # subdomain
+        subdomains=None
         if value2domian !=None:
             subdomain_thread_num=subdomain_scan["thread"]
-            subdomain_file_path=subdomain_scan["file_path"]
-            subdomain_file_name=subdomain_scan["file_name"]
+            if subdomain_scan["mode"]=="file":
+                subdomain_file_name=subdomain_scan["file_name"]
+            elif subdomain_scan["mode"]=="dir":
+                subdomain_file_path=subdomain_scan["file_path"]
             subdomain_scanner=subdomain.subdomain_scanner(value2domian[4:],threads=subdomain_thread_num,file_path=subdomain_file_path,file_name=subdomain_file_name)
-            subdomains=subdomains=subdomain_scanner.get_subdomains()
+            subdomains=subdomain_scanner.get_subdomains()
+        if g_debug==1:
+            loguru.logger.info(f"get subdomains ==> subdomains is {subdomains}")
 
         # web file
         web_path_scan_mode=web_path_scan["mode"]
         thread_num=web_path_scan["thread"]
+        if web_path_scan_mode=="file":
+            web_path_file_name=web_path_scan["file_name"]
+        elif web_path_scan_mode=="dir":
+            web_path_file_path=web_path_scan["file_path"]
         web_path=None
         if web_path_scan_mode=="file":
-            web_path=web_path_scanner.scanner(value,file_name="db/dicc.txt",threads=thread_num)
+            web_path=web_path_scanner.scanner(value,file_name=web_path_file_name,threads=thread_num)
         elif web_path_scan_mode=="dir":
-            web_path=web_path_scanner.scanner(value,file_path="db",threads=thread_num)
+            web_path=web_path_scanner.scanner(value,file_path=web_path_file_path,threads=thread_num)
         
         # Fingerprint collection
         no_scan_ports=["20","21","22","25","53","80","110","143","443","1433","3389"]
         fingerprint=None
         tide=[]
-        if g_debug==1:
-            ports=["8080"]
+        # if g_debug==1:
+        #     ports=["8080"]
         for port in ports:
             if port in no_scan_ports:
+                loguru.logger.info(f"port {port} is not scan")
                 continue
             fingerprint=TideFinger.run(value,port)  # return is a dict
             if len(fingerprint["banner"]) != 0:
                 tide+=fingerprint["banner"]
             if fingerprint["cms_name"]!="Not Found":
                 tide.append(fingerprint["cms_name"])
-        
+        if g_debug==1:
+            loguru.logger.info(f"Fingerprint collection ==> fingerprint is {fingerprint}")
+
         # Vulnerability detection
         for port in ports:
             Vulmap.start(value,str(port))
         vuls=read_vuls.read_data("ret/vuls.txt")
+        if g_debug==1:
+            loguru.logger.info(f"Vulnerability detection ==> vuls is {vuls}")
 
         # crete info data
         info_data={}
@@ -257,6 +357,9 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
         info_data["web_path"]=web_path # web_path is a list
         info_data["domain"]=value2domian # domain is a string
         info_data["subdomains"]=subdomains # subdomains is a list
+        if g_debug==1:
+            loguru.logger.info(f"create info data ==> info_data is {info_data}")
+
 
         # send info data to es
         use_es=ES.MyElasticSearch(es["es_host"],es["es_port"])
@@ -270,7 +373,9 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
             use_es.insert_data(es["es_info_index"],info_data)
         except Exception as e:
             loguru.logger.error(e)
-            
+        if g_debug==1:
+            loguru.logger.info("send info data to es")
+
         # crete vuls data
         for vul in vuls:
             vul_data={}
@@ -291,6 +396,8 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
             except Exception as e:
                 loguru.logger.error(e)
                 continue
+            if g_debug==1:
+                loguru.logger.info(f"send vuls is {vul_data}")
 
         # notice
         if g_debug==1:
@@ -304,26 +411,6 @@ def worker(redis: my_redis.Redis,es:dict,web_path_scan:dir=None,subdomain_scan:d
                 loguru.logger.info("clear vuls.txt")
         except Exception as e:
             loguru.logger.error(e)
-            
-                
-def check_redis(redis_host, redis_port,redis_password):
-    # each 5 seconds, try to connect redis, and connect 100 times 
-    now_times=0
-    max_times=100
-
-    ip_tools.check_ip_if_valid(redis_host)
-
-    try:
-        while now_times<max_times:
-            if my_redis.Redis.check_if_connectable(redis_host, redis_port, redis_password):
-                return
-            loguru.logger.info("can't connect redis")
-            now_times+=1
-            time.sleep(5)
-        raise Exception
-    except:
-        loguru.logger.error("redis connect error")
-        sys.exit(1)
 
 
 def main():
@@ -352,19 +439,19 @@ def main():
     # run worker
     worker(redis=redis,es=es,web_path_scan=wps,subdomain_scan=subdomain)
 
-def test(ip:str):
-    # ret=TideFinger.run(ip,port)
-    # print(ret)
+def test(ip:str,port):
+    ret=TideFinger.run(ip,str(port))
+    print(ret)
     # web_path_scanner.scanner(ip,file_path="db",threads=10)
     # test domain
-    t=domain.run(ip)
-    print(t[4:])
-    # test subdomain
-    scanenr=subdomain.subdomain_scanner(t[4:],threads=10,file_name="data/subdomain/dict.txt")
-    print(len(scanenr.get_subdomains()))
+    # t=domain.run(ip)
+    # print(t[4:])
+    # # test subdomain
+    # scanenr=subdomain.subdomain_scanner(t[4:],threads=10,file_name="data/subdomain/dict.txt")
+    # print(len(scanenr.get_subdomains()))
 
 if __name__=="__main__":
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
     # main()
-    test('218.76.8.98')
+    test('192.168.79.128',8080)
